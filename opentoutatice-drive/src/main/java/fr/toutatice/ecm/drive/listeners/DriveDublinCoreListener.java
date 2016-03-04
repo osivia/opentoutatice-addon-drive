6da8386a05433c9b2920e3343de72310b7005e0a
no_author <contact@osivia.com>
2016-03-04 10:41:03 +0000

    New Fix for #1098

--- opentoutatice-drive/src/main/java/fr/toutatice/ecm/drive/listeners/DriveDublinCoreListener.java
+++ opentoutatice-drive/src/main/java/fr/toutatice/ecm/drive/listeners/DriveDublinCoreListener.java
@@ -7,6 +7,7 @@ import org.nuxeo.drive.service.impl.NuxeoDriveManagerImpl;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
+import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.platform.dublincore.listener.DublinCoreListener;
@@ -30,9 +31,12 @@ public class DriveDublinCoreListener extends DublinCoreListener {
             return;
         }
 		if(sourceDocument != null){
-			if(DocumentEventTypes.BEFORE_DOC_UPDATE.equals(eventName)
+			if(DocumentEventTypes.BEFORE_DOC_UPDATE.equals(eventName) 
 					&& sourceDocument.hasFacet(NuxeoDriveManagerImpl.NUXEO_DRIVE_FACET)){
-				return;
+				Property driveSubscription = sourceDocument.getProperty(NuxeoDriveManagerImpl.DRIVE_SUBSCRIPTIONS_PROPERTY);
+				if(driveSubscription.isDirty()){
+					return;
+				}
 			}
 		}
 		
